f1b5414f89f864a302f9c01f491084c2445644d9
ar230500 <60130224+ar230500@users.noreply.github.com>
2023-04-19 11:20:46 +0500

    Update app.py

--- app.py
+++ app.py
@@ -28,7 +28,7 @@ def add_user():
     cluster.insert_one(user)
     return "User Added"
 
-if __name__ == '__main__':
-    #port = int(os.environ.get('PORT', 5000))
-    print(os.getcwd())
-    #app.run(host='0.0.0.0', port=port)
+# if __name__ == '__main__':
+#     #port = int(os.environ.get('PORT', 5000))
+#     print(os.getcwd())
+#     #app.run(host='0.0.0.0', port=port)
