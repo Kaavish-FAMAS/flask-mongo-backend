4c1a9bd2dcbaca997d41eba46ee85552e54af31e
ar230500 <60130224+ar230500@users.noreply.github.com>
2023-04-19 11:25:42 +0500

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ cluster = db.users
 @app.route("/")
 def home():
     #print(client.Authentication.users.find_one({"_id": "123456789"}))
-    return "Hello, Flask!"
+    return "Checking Vercel"
 
 @app.route("/getuser/", methods=['GET', 'POST'])
 def get_user():
